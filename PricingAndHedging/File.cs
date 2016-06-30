3676ea6e97279b1e2bdc08dff8b1237f1ef56c70
Igor Aquino <igoraquino@users.noreply.github.com>
2016-06-30 19:53:21 -0300

    Create File.cs

---
+++ PricingAndHedging/File.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace PricingAndHedging
+{
+    public static class BmfCalendar
+    {
+        private static List<DateTime> Holidays = new List<DateTime>()
+        {            
+            new DateTime(2014, 1, 1),
+            new DateTime(2014, 1, 25),
+            new DateTime(2014, 6, 12),
+            new DateTime(2014, 7, 9),
+            new DateTime(2014, 11, 20),
+            new DateTime(2014, 12, 24),
+            new DateTime(2014, 12, 31),
+            new DateTime(2015, 1, 1),
+            new DateTime(2015, 1, 25),
+            new DateTime(2015, 7, 9),
+            new DateTime(2015, 11, 20),
+            new DateTime(2015, 12, 24),
+            new DateTime(2015, 12, 31),
+            new DateTime(2016, 1, 1),
+            new DateTime(2016, 1, 25),
+            new DateTime(2016, 7, 9),
+            new DateTime(2016, 11, 20),
+            new DateTime(2016, 12, 24),
+            new DateTime(2016, 12, 31)
+        };
+
+        public static bool IsBusinessDate(DateTime date)
+        {
+            if ((date.DayOfWeek == DayOfWeek.Saturday) || (date.DayOfWeek == DayOfWeek.Sunday))
+                return false;
+
+            if (Holidays.Contains(date))
+                return false;
+
+            return true;
+        }
+    }
+}
