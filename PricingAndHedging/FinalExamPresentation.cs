c8bd88b366b3dc92baeaf6e164986a564f0a1e2b
Igor Aquino <igoraquino@users.noreply.github.com>
2016-06-30 19:48:22 -0300

    Update FinalExamPresentation.cs

--- PricingAndHedging/FinalExamPresentation.cs
+++ PricingAndHedging/FinalExamPresentation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,27 +17,52 @@ namespace PricingAndHedging.FinalExam
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void InterpolationExperiments()
         {
-            var vols = new VOLS();
-            var vola = vols[new DateTime(2014, 7, 1)];
-            MessageBox.Show(vola.GetVol(new DateTime(2014, 9, 10)).ToString());
+            MessageBox.Show("vols " + VOLS.GetVol(new DateTime(2014, 7, 1), new DateTime(2014, 9, 10)).ToString());
 
-
-            var fwds = new FWDS();
-            MessageBox.Show("fwds " + fwds[new DateTime(2014, 7, 1)].GetFwd(new DateTime(2014, 7, 10)).ToString());
-
-            var f = new ForwardsForDate(new DateTime(2014, 7, 1), 2.2018, 2.2199, 2.2557, 2.4148);
-            MessageBox.Show(f.GetFwd(new DateTime(2014, 7, 10)).ToString());
-
-            var rates = new RATES();
-            var r = rates[new DateTime(2014, 7, 1)];
+            MessageBox.Show("fwds " + FWDS.GetFwd(new DateTime(2014, 7, 1),new DateTime(2014, 7, 10)).ToString());
 
             var ir = new InterestRatesForDate(new DateTime(2014, 7, 1), 0.1081, 0.107741, 0.108398);
-            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2014, 7, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 7, 10)).ToString());
-            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2014, 9, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 9, 10)).ToString());
-            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2014, 10, 1)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 10, 1)).ToString());
-            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2015, 5, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2015, 5, 10)).ToString());
+            MessageBox.Show("rates: " + RATES.GetRate(new DateTime(2014, 7, 1), new DateTime(2014, 7, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 7, 10)).ToString());
+            MessageBox.Show("rates: " + RATES.GetRate(new DateTime(2014, 7, 1), new DateTime(2014, 9, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 9, 10)).ToString());
+            MessageBox.Show("rates: " + RATES.GetRate(new DateTime(2014, 7, 1), new DateTime(2014, 10, 1)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 10, 1)).ToString());
+            MessageBox.Show("rates: " + RATES.GetRate(new DateTime(2014, 7, 1), new DateTime(2015, 5, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2015, 5, 10)).ToString());
+        }
+
+        private void Experiments()
+        {
+            MessageBox.Show(BmfCalendar.IsBusinessDate(DateTime.Today.AddDays(2)).ToString());
+            MessageBox.Show(BmfCalendar.IsBusinessDate(DateTime.Today).ToString());
+            MessageBox.Show(BmfCalendar.IsBusinessDate(new DateTime(1991, 01, 01)).ToString());            
+        }
+
+        private void HedgedPortfolio()
+        {
+            var tradeDate = new DateTime(2015, 06, 30);
+            var maturity = tradeDate.AddMonths(1);
+
+            var portfolioByDate = new Dictionary<DateTime, Portfolio>();
+
+
+
+            var currentDay = tradeDate;
+            while ((currentDay < maturity))
+            {
+                if (!BmfCalendar.IsBusinessDate(currentDay))
+                {
+                    currentDay.AddDays(1);
+                    continue;
+                }
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.InterpolationExperiments();
+            this.Experiments();
         }
     }
 }
