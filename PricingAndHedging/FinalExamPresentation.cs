62b24b084c0bb89857be09cd00ba9806aadfd3c4
igoraquino <igoraquino@gmail.com>
2016-06-30 01:58:25 -0300

    Classes with static data for interest rate, USDBRL forward and volatility

--- PricingAndHedging/FinalExamPresentation.cs
+++ PricingAndHedging/FinalExamPresentation.cs
@@ -19,20 +19,25 @@ namespace PricingAndHedging.FinalExam
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var v = new VolatilitiesForDate(new DateTime(2014, 7, 1), 0.09025, 0.10158, 0.1237);
-            var vol = v.GetVol(new DateTime(2014, 9, 10));
+            var vols = new VOLS();
+            var vola = vols[new DateTime(2014, 7, 1)];
+            MessageBox.Show(vola.GetVol(new DateTime(2014, 9, 10)).ToString());
+
+
+            var fwds = new FWDS();
+            MessageBox.Show("fwds " + fwds[new DateTime(2014, 7, 1)].GetFwd(new DateTime(2014, 7, 10)).ToString());
 
             var f = new ForwardsForDate(new DateTime(2014, 7, 1), 2.2018, 2.2199, 2.2557, 2.4148);
             MessageBox.Show(f.GetFwd(new DateTime(2014, 7, 10)).ToString());
-            MessageBox.Show(f.GetFwd(new DateTime(2014, 9, 10)).ToString());
-            MessageBox.Show(f.GetFwd(new DateTime(2014, 10, 1)).ToString());
-            MessageBox.Show(f.GetFwd(new DateTime(2015, 5, 10)).ToString());
+
+            var rates = new RATES();
+            var r = rates[new DateTime(2014, 7, 1)];
 
             var ir = new InterestRatesForDate(new DateTime(2014, 7, 1), 0.1081, 0.107741, 0.108398);
-            MessageBox.Show(ir.GetCDI(new DateTime(2014, 7, 10)).ToString());
-            MessageBox.Show(ir.GetCDI(new DateTime(2014, 9, 10)).ToString());
-            MessageBox.Show(ir.GetCDI(new DateTime(2014, 10, 1)).ToString());
-            MessageBox.Show(ir.GetCDI(new DateTime(2015, 5, 10)).ToString());
+            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2014, 7, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 7, 10)).ToString());
+            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2014, 9, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 9, 10)).ToString());
+            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2014, 10, 1)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2014, 10, 1)).ToString());
+            MessageBox.Show("rates: " + r.GetCDI(new DateTime(2015, 5, 10)).ToString() + "\t\t" + ir.GetCDI(new DateTime(2015, 5, 10)).ToString());
         }
     }
 }
